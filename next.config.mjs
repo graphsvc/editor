4e276d8398607f2b71822751302a51e9bcfff843
Matthew Ratzke <matt@degree9.io>
2023-03-03 19:38:39 -0700

    update deps

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+export const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  // transpilePackages: [
+    // '@next-auth/firebase-adapter',
+    // '@apollo/sandbox'
+  // ],
+};
+
+export default nextConfig;
